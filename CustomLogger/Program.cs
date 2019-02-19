1adf3a464843367d0b5588e070123d4db5b16554
rajnishoecrajnishmsft2015@outlook.com <rajnishoecrajnishmsft2015@outlook.com>
2019-02-20 02:06:08 +0530

    C# Logger

---
+++ CustomLogger/Program.cs
@@ -0,0 +1,15 @@
+﻿[assembly:log4net.Config.XmlConfigurator(Watch =true)]
+namespace CustomLogger
+{
+    public class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();/*log4net.LogManager.GetLogger
+ (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);*/
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Writing log...");
+            log.Error("This is my error message");
+            System.Console.ReadLine();
+        }
+    }
+}
